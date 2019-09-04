e2a414840c0f69ec1e0de0d26a88a1e5cc9eeb54
sarawall1187 <sarawall1187@gmail.com>
2019-09-04 08:37:05 -0700

    added sort button per final assessment live coding exercise

--- src/components/MyEntries.js
+++ src/components/MyEntries.js
@@ -7,7 +7,8 @@ class MyEntries extends React.Component {
     constructor(){
         super()
         this.state = {
-            votes: {}
+            votes: {},
+            sortClicked: false
         }
     }
 
@@ -43,21 +44,45 @@ class MyEntries extends React.Component {
         }
       })
     }
+
+    sorted = () => {
+       let entriesCopy = this.props.myEntries.slice()
+        entriesCopy.sort((a, b) => {
+            let aDate = new Date(a.attributes.created_at)
+            let bDate = new Date(b.attributes.created_at)
+           return bDate - aDate
+        })
+       return entriesCopy 
+    }
+
+    handleClick = () => {
+        this.setState({
+            sortClicked: true
+        })
+    }
     
 
  render() {
+   let entriesToDisplay = this.props.myEntries
+    if (this.state.sortClicked) {
+         entriesToDisplay = this.sorted()
+    } 
     return (
      <div className="entryContainer active">
-        {this.props.myEntries.map(entry => 
+     <button onClick={this.handleClick}>Sort Entries</button>
+        {entriesToDisplay.map(entry => 
             <> 
             <Link to={`entries/${entry.id}`} key={entry.id}>
                 <h3>Diary Entry for: {entry.attributes.created_at}</h3>
             </Link>
             <button onClick={() => this.upVoted(entry.id)}>Upvote {this.state.votes[entry.id]}+</button>
+            
             </>
             )}
      </div>
+     
     )
+  
   }
 }
 
